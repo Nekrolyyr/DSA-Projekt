f49a9d14eea88f3e44bf3982ca3abbb9e506da28
Martin Sauter <msauter@hsr.ch>
2019-10-16 08:23:18 +0200

    Merged some shit

---
+++ src/hsr/dsa/core/game/schiffe_versenken/SchiffeVersenken.java
@@ -0,0 +1,6 @@
+package hsr.dsa.core.game.schiffe_versenken;
+
+import hsr.dsa.core.game.Game;
+
+public class SchiffeVersenken extends Game {
+}
---
+++ src/hsr/dsa/gui/game/Field.java
@@ -0,0 +1,19 @@
+package hsr.dsa.gui.game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Field extends JButton {
+
+
+    public Field() {
+        this.setBackground(Color.lightGray);
+        this.addActionListener(actionEvent -> fieldClicked());
+    }
+
+    public void fieldClicked() {
+        this.setBackground(Color.blue);
+        this.setEnabled(false);
+    }
+
+}
