a9af6fcf681885509dc47e8d317a4714b0b07c9a
David <dzwick@hsr.ch>
2019-10-20 11:24:45 +0200

    Testing works

--- src/hsr/dsa/Launcher.java
+++ src/hsr/dsa/Launcher.java
@@ -1,31 +1,17 @@
 package hsr.dsa;
 
-import net.tomp2p.futures.BaseFuture;
-import net.tomp2p.futures.BaseFutureListener;
-import net.tomp2p.futures.FutureDHT;
-import net.tomp2p.futures.FutureResponse;
-import net.tomp2p.message.Message;
-import net.tomp2p.p2p.Peer;
-import net.tomp2p.p2p.PeerMaker;
-import net.tomp2p.peers.Number160;
-import net.tomp2p.storage.Data;
-
-import java.io.IOException;
-import java.net.InetAddress;
+import java.util.Collections;
 
 public class Launcher {
-    public static void main(String[] args) {
-        Peer peer = null;
-        try {
-            peer = new PeerMaker(new Number160(0xffaa)).setPorts(43001).makeAndListen();
-            //peer.discover().setInetAddress(InetAddress.getLocalHost()).start();
-            peer.put(Number160.createHash("David")).setData(new Data("Student")).start();
-            FutureDHT f = peer.get(Number160.createHash("David")).start();
-            f.await();
-            System.out.println(f.getData());
-        }catch(Exception e){
-            e.printStackTrace();
-            if(peer!=null)peer.shutdown();
-        }
+    public static void main(String[] args) throws NumberFormatException, Exception {
+        ExampleSimple dns1 = new ExampleSimple(1);
+        ExampleSimple dns2 = new ExampleSimple(2);
+        ExampleSimple dns3 = new ExampleSimple(3);
+        dns1.store("David","192.168.1.101");
+        dns2.store("Nekrolyyr","192.168.1.102");
+        System.out.println(dns3.get("David"));
+        System.out.println(dns3.get("Nekrolyyr"));
+        dns1.send(dns2.peer.peerAddress(),"Hallo Welt");
+        System.out.println(dns1.discoverPeers());
     }
 }
