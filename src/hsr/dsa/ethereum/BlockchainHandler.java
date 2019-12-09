5dbf73a7d7af01a0b07a088ff5eb9464fa691a3e
Martin Sauter <msauter@hsr.ch>
2019-12-09 21:59:46 +0100

    Ethereum stuff unblocking implemented

--- src/hsr/dsa/ethereum/BlockchainHandler.java
+++ src/hsr/dsa/ethereum/BlockchainHandler.java
@@ -18,18 +18,16 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class BlockchainHandler {
-    private static final String SMART_CONTRACT_ADDRESS = "0x8D18D6144d338079cD4b4f7D0981f4eE7477D49f";
 
+    private static final String SMART_CONTRACT_ADDRESS = "0x8D18D6144d338079cD4b4f7D0981f4eE7477D49f";
     private SmartContractDSAProject smartContract;
     private String localEtherAccount;
     private String remoteEtherAccount;
     private String localPrivateKey;
-
     @SuppressWarnings("FieldCanBeLocal")
     private String testNetAddress = "https://rinkeby.infura.io/";
     private Web3j localWeb3;
     private Web3j smartContractWeb3;
-
     private String davidsEtherAccount = "0x1cE0089b18c8135B6fff8b10fC43F596A7289D83";
     private String martinsEtherAccount = "0x036FBAE35b84e03926Cf466C2Ef19165C66829b2";
     private String martinsPrivateKey = "d458a482cb2d7532aab8f76994a32351d5190bc08d661636690fae7272efeaac"; // TODO: Delete private key
@@ -63,72 +61,18 @@ public class BlockchainHandler {
     }
 
     public void loadDeployedSmartContract() {
-        Credentials creds = Credentials.create(localPrivateKey);
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                smartContract = SmartContractDSAProject.load(SMART_CONTRACT_ADDRESS, smartContractWeb3, creds, new DefaultGasProvider());
-
-                try {
-                    if (!smartContract.isValid()) {
-                        System.out.println("Could not load smart contract!!");
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-
+        new SmartContractLoader().execute();
     }
 
     public void storeAmountInBlockchain(BigDecimal gambleAmountETH) throws Exception {
-        BigInteger gambleAmountWEI = gambleAmountETH.multiply(new BigDecimal("1E18")).toBigInteger();
-        if (gambleAmountWEI.compareTo(getBalanceFromAccount(localEtherAccount)) != -1) {
-            System.out.println("You have not enough ethers!");
-            throw new Exception("You have not enough ethers!");
-        }
-        if (gambleAmountWEI.compareTo(getBalanceFromAccount(remoteEtherAccount)) != -1) {
-            System.out.println("Your enemy has not enough ether!");
-            throw new Exception("Your enemy has not enough ether!");
-        }
-
-
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                TransactionReceipt loadAmountToContract = null;
-                try {
-                    loadAmountToContract = smartContract.start(gambleAmountWEI).send();
-                    if (!loadAmountToContract.isStatusOK()) {
-                        System.out.println("Could not load ethers into the smart Contract!!");
-                        throw new Exception("Could not load ethers into the smart Contract!!");
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
-
+        StoreAmountHandler handler = new StoreAmountHandler();
+        handler.setGambleAmountETH(gambleAmountETH);
+        handler.execute();
     }
 
     // Can only be called from the looser, because he has to pay the winner.
     public void startTransaction() {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    TransactionReceipt payAmount = smartContract.payAmountToEnemty(remoteEtherAccount).send();
-                    if (!payAmount.isStatusOK()) {
-                        System.out.println("Could not pay ethers to the winner!");
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
+        new TransactionHandler().execute();
     }
 
     public void printClientVersions() {
@@ -153,5 +97,73 @@ public class BlockchainHandler {
     public void printGasPrice() {
         System.out.println("Gas price: " + DefaultGasProvider.GAS_PRICE);
         System.out.println("Gas limit: " + DefaultGasProvider.GAS_LIMIT);
+    }
+
+    class TransactionHandler extends SwingWorker<Void, Void> {
+        @Override
+        protected Void doInBackground() throws Exception {
+            try {
+                TransactionReceipt payAmount = smartContract.payAmountToEnemty(remoteEtherAccount).send();
+                System.out.println("Amount payed");
+                if (!payAmount.isStatusOK()) {
+                    System.out.println("Could not pay ethers to the winner!");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+    class StoreAmountHandler extends SwingWorker<Void, Void> {
+        BigDecimal gambleAmountETH;
+
+        void setGambleAmountETH(BigDecimal amount) {
+            gambleAmountETH = amount;
+        }
+
+        @Override
+        protected Void doInBackground() throws Exception {
+            BigInteger gambleAmountWEI = gambleAmountETH.multiply(new BigDecimal("1E18")).toBigInteger();
+            if (gambleAmountWEI.compareTo(getBalanceFromAccount(localEtherAccount)) != -1) {
+                System.out.println("You have not enough ethers!");
+                throw new Exception("You have not enough ethers!");
+            }
+            if (gambleAmountWEI.compareTo(getBalanceFromAccount(remoteEtherAccount)) != -1) {
+                System.out.println("Your enemy has not enough ether!");
+                throw new Exception("Your enemy has not enough ether!");
+            }
+
+            TransactionReceipt loadAmountToContract = null;
+            try {
+                loadAmountToContract = smartContract.start(gambleAmountWEI).send();
+                System.out.println("Amount stored");
+                if (!loadAmountToContract.isStatusOK()) {
+                    System.out.println("Could not load ethers into the smart Contract!!");
+                    throw new Exception("Could not load ethers into the smart Contract!!");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    class SmartContractLoader extends SwingWorker<Void, Void> {
+        @Override
+        protected Void doInBackground() throws Exception {
+            Credentials creds = Credentials.create(localPrivateKey);
+
+            smartContract = SmartContractDSAProject.load(SMART_CONTRACT_ADDRESS, smartContractWeb3, creds, new DefaultGasProvider());
+            System.out.println("Smart contract loaded!");
+
+            try {
+                if (!smartContract.isValid()) {
+                    System.out.println("Could not load smart contract!!");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
